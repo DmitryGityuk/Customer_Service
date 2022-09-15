989a0f0f24c7616e357112f774550c41bd9bfab2
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-09-15 18:13:26 +0500

    updated Order

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 import java.util.Set;
 
 @Document(collection = "order")
@@ -64,4 +65,17 @@ public class Order implements Serializable {
     @Field("products")
     @NotEmpty
     private Set<@Valid Product> products;
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id.equals(order.id);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
 }
